b21a4886f334202e45f205c144b054c36510c85b
nhubtt <nhubtt@vng.com.vn>
2023-11-19 21:52:33 +0700

    handle event mouse enter and mouse leave for icons

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -62,6 +62,46 @@ export default function MediaInfo() {
     return null;
   }
 
+  const onMouseEnterAction = (type: 'favorite' | 'share' | 'embed') => {
+    switch (type) {
+      case 'favorite':
+        document.getElementById('favorite-icon')?.classList.add('heart-icon');
+        break;
+
+      case 'share':
+        document.getElementById('share-icon')?.classList.add('send-icon');
+        break;
+
+      case 'embed':
+        document.getElementById('embed-icon')?.classList.add('code-icon');
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const onMouseLeaveAction = (type: 'favorite' | 'share' | 'embed') => {
+    switch (type) {
+      case 'favorite':
+        document
+          .getElementById('favorite-icon')
+          ?.classList.remove('heart-icon');
+        break;
+
+      case 'share':
+        document.getElementById('share-icon')?.classList.remove('send-icon');
+        break;
+
+      case 'embed':
+        document.getElementById('embed-icon')?.classList.remove('code-icon');
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="flex items-start gap-20 grid-cols-2 w-full pt-10">
       {renderAvatar()}
@@ -69,16 +109,28 @@ export default function MediaInfo() {
       <div className="flex items-center gap-20">
         {renderImage()}
         <div className="flex flex-col gap-4">
-          <div className="flex gap-4 cursor-pointer">
-            <HeartFilled style={{ fontSize: '20px' }} className="heart-icon" />
+          <div
+            className="flex gap-4 cursor-pointer"
+            onMouseEnter={() => onMouseEnterAction('favorite')}
+            onMouseLeave={() => onMouseLeaveAction('favorite')}
+          >
+            <HeartFilled style={{ fontSize: '20px' }} id="favorite-icon" />
             Favorite
           </div>
-          <div className="flex gap-4 cursor-pointer">
-            <SendOutlined style={{ fontSize: '20px' }} className="send-icon" />
+          <div
+            className="flex gap-4 cursor-pointer share"
+            onMouseEnter={() => onMouseEnterAction('share')}
+            onMouseLeave={() => onMouseLeaveAction('share')}
+          >
+            <SendOutlined style={{ fontSize: '20px' }} id="share-icon" />
             Share
           </div>
-          <div className="flex gap-4 cursor-pointer">
-            <CodeOutlined style={{ fontSize: '20px' }} className="code-icon" />
+          <div
+            className="flex gap-4 cursor-pointer embed"
+            onMouseEnter={() => onMouseEnterAction('embed')}
+            onMouseLeave={() => onMouseLeaveAction('embed')}
+          >
+            <CodeOutlined style={{ fontSize: '20px' }} id="embed-icon" />
             Embed
           </div>
         </div>
